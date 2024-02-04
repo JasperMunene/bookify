c59e87056d977c764448428cd2102d0bf9155e44
Jasper <jaspermunene2022@gmail.com>
2024-02-04 15:36:28 +0300

    Connection to the postgeSQL database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import pg from 'pg';
 import cors from 'cors'
+import env from 'dotenv'
 
 const app = express();
 const port = process.env.PORT || 3000
+env.config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//Databse connection
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT) || 5432,
+});
+
+db.connect()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database:', err);
+        process.exit(1);
+    });
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
